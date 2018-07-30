4bdd58188f899816987cc6334e010e132ac8638d
Matt Tuchfarber <matt@tuchfarber.com>
2018-07-30 10:32:03 -0400

    Add page navigation to JournalPage

--- src/containers/JournalPageContainer/index.jsx
+++ src/containers/JournalPageContainer/index.jsx
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import JournalPage from '../../components/JournalPage';
 import fetchPage from '../../data/actions/page';
@@ -9,6 +10,8 @@ const mapStateToProps = state => (
     title: state.page.page.title,
     body: state.page.page.body,
     fetchPageSuccess: state.page.error,
+    nextPage: state.page.page.next_page_id,
+    previousPage: state.page.page.previous_page_id,
   }
 );
 
@@ -23,4 +26,5 @@ const JournalPageContainer = connect(
   mapDispatchToProps,
 )(JournalPage);
 
-export default JournalPageContainer;
+
+export default withRouter(JournalPageContainer);
