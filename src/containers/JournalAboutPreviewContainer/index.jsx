986505be56d34514926f8f98b1dc8a27ba577074
hasnain-naveed <hasnain.naveed123@gmail.com>
2019-01-18 13:19:20 +0500

    WL-1868 | Added jest test cases for JournalAboutPage and JournalAboutPreview components.

--- src/containers/JournalAboutPreviewContainer/index.jsx
+++ src/containers/JournalAboutPreviewContainer/index.jsx
@@ -6,7 +6,18 @@ import fetchPreview from '../../data/actions/preview';
 
 const mapStateToProps = state => (
   {
-    previewPage: state.preview.page,
+    previewPage: {
+      title: state.preview.page.title,
+      shortDescription: state.preview.page.short_description,
+      longDescription: state.preview.page.long_description,
+      heroImageUrl: state.preview.page.hero_image_url,
+      customContent: state.preview.page.custom_content,
+      id: state.preview.page.id,
+      journalId: state.preview.page.journal_id,
+      purchaseUrl: state.preview.page.purchase_url,
+      price: state.preview.page.price,
+      accessLength: state.preview.page.accessLength,
+    },
     finishedFetching: state.preview.finishedFetching,
     authorizedJournals: state.siteInfo.authorizedJournals,
     serverBaseUrl: state.siteInfo.serverBaseUrl,
